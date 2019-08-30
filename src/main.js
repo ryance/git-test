dde00388942e59db01386600187cd9fb1e447965
ryn <ryance_h@sina.com>
2019-08-30 17:17:20 +0800

    重装后提交

---
+++ src/main.js
@@ -0,0 +1,12 @@
+//使用main.js导入包和静态资源，可以加快网页加载速度，减少二次请求
+
+//导入jQuery
+//import *** from *** 是ES6中导入模块的方式
+import $ from 'jquery'
+
+$(function() {
+    $('li:odd').css('backgroundColor', 'green')
+    $('li:even').css('backgroundColor', function() {
+        return '#' + 'D97634'
+    })
+})
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+//这个配置文件，其实就是一个js文件，通过Node中的模块操作，向外暴露了一个配置对象
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, './src/main.js'), //输入文件路径
+    output: {
+        path: path.join(__dirname, './dist'), //输出文件路径
+        filename: 'bundle.js' //指定输出文件名称
+    }
+
+
+}

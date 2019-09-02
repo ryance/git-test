1e635fdffb17c02ed39d5c428a837d91fcd8c8a1
ryn <ryance_h@sina.com>
2019-09-02 15:24:14 +0800

    9-2学习

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@
 //导入jQuery
 //import *** from *** 是ES6中导入模块的方式
 import $ from 'jquery'
+//webpack默认只能打包处理JS类型的文件，如果要处理其他非JS类型的文件，需要安装其他loader
+
+import './css/index.css'
+
+import './css/index.less'
 
 $(function() {
-    $('li:odd').css('backgroundColor', 'darkcyan')
+    $('li:odd').css('backgroundColor', 'blue')
     $('li:even').css('backgroundColor', function() {
         return '#' + 'D97634'
     })
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 //这个配置文件，其实就是一个js文件，通过Node中的模块操作，向外暴露了一个配置对象
 module.exports = {
@@ -8,7 +9,24 @@ module.exports = {
         path: path.join(__dirname, './dist'), //输出文件路径
         filename: 'bundle.js', //指定输出文件名称
 
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, './index.html'), //指定模板文件，根据指定文件生成内存里的文件
+            filename: 'index.html', //指定生成的页面文件名称
+
+        })
+    ],
+    module: { //用于配制所有的第三方模块加载器
+        rules: [ //第三方模块的匹配规则
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }, //配制处理.css文件的loader规则
+            //webpack调用loader的顺序是从后往前，分别调用后一个loader再把结果传到其前一个loader继续处理
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            //处理less文件的顺序是less-css-style
+        ]
+
     }
+
 
 
 }

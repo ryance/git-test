89c3d0db2fc45f7d0a956716bc8e7f86920ff6b7
ryn <ryance_h@sina.com>
2019-09-02 17:23:03 +0800

    9.2学习

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
             //webpack调用loader的顺序是从后往前，分别调用后一个loader再把结果传到其前一个loader继续处理
             { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
             //处理less文件的顺序是less-css-style
+            { test: /\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader?limit=1930360&name=[hash:8]-[name].[ext]' },
+            //file-loader是内部依赖，不需要在use中引入
+            // 可以加参数，参数格式和url参数一致，但参数名是固定的
+            // limit给定的值是图片的大小，单位是byte。如果引用的图片大于等于这个值，则不会被转为base64格式的字符串，反之。
+            // name用来规定打包后的文件名。无参时会把文件名哈希重命名防止重名，上述用来改命名规则为8位哈希值-原文件名
+            // url-loader同样可以用来处理字体文件，但也需要添加rules。 
+            { test: /\.ttf$/, use: 'url-loader' },
         ]
 
     }
